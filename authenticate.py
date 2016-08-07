db592a00cf2ffb3c8cc60d977349e2540cb455b7
Aloys Augustin <aloys.augustin@polytechnique.org>
2016-08-06 17:32:00 -0700

    Auth

--- authenticate.py
+++ authenticate.py
@@ -6,9 +6,6 @@ users = {
     'dummy.user': 'crapineedasigningkey'
 }
 
-def sign(key, msg):
-    return hmac.new(key, msg.encode('utf-8'), hashlib.sha256).digest()
-
 def getSignatureKey(key, dateStamp, regionName, serviceName):
     kDate = sign(('AWS4' + key).encode('utf-8'), dateStamp)
     kRegion = sign(kDate, regionName)
@@ -21,17 +18,43 @@ def extract_from_auth(header, desc):
     if begin == -1:
         raise LookupError
     begin += len(desc) + 1
-    return header[begin:header.find(',', begin)]
+    end = header.find(',', begin)
+    if end == -1:
+        return header[begin:]
+    else:
+        return header[begin:end]
 
 def authenticate(request):
     host = request.headers['Host']
     auth = request.headers['Authorization'].split(' ')
+    amz_date = request.headers['X-Amz-Date']
     print 'Auth:', auth
-    sig_version = auth[0]
-    if sig_version != 'AWS4-HMAC-SHA256':
+    algorithm = auth[0]
+    if algorithm != 'AWS4-HMAC-SHA256':
         return 'Error: unsupported signing scheme'
     cred = extract_from_auth(auth, 'Credential')
+    key_id, credential_scope = cred.split('/', 1)
+    date_stamp, region, service, _ = credential_scope.split('/')
+    if not key_id in users:
+        return None
+
+    method = request.method
+    amz_target = request.headers['X-Amz-Target']
+    request_parameters = request.data
+    secret_key = users[key_id]
     signed_headers = extract_from_auth(auth, 'SignedHeaders')
-    return None
+    given_sig = extract_from_auth(auth, 'Signature')
+
+    canonical_uri = request.path
+    canonical_querystring = request.query_string
+    canonical_headers = '\n'.join(k.lower() + ': ' + v for k, v in sorted(request.headers.items()) if k in signed_headers.split(';')) + '\n'
+    payload_hash = hashlib.sha256(request_parameters).hexdigest()
+    canonical_request = method + '\n' + canonical_uri + '\n' + canonical_querystring + '\n' + canonical_headers + '\n' + signed_headers + '\n' + payload_hash
+    string_to_sign = algorithm + '\n' +  amzdate + '\n' +  credential_scope + '\n' +  hashlib.sha256(canonical_request).hexdigest()
+    signing_key = getSignatureKey(secret_key, datestamp, region, service)
+    computed_sig = hmac.new(signing_key, (string_to_sign).encode('utf-8'), hashlib.sha256).hexdigest()
+    if computed_sig != given_sig:
+        return None
+    return 
 
 
